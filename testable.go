c938232b6b6a42c72e85cddeea0712264730a14f
iannucci <iannucci@chromium.org>
2015-07-13 18:30:40 -0700

    Refactor current GAE abstraction library to be free of the SDK*
    Also improves the struct serialization implementation over the SDK's:
      * 'non-struct' representation is now map[string][]values, instead of
        []PropertyList. This makes working with PropertyLoadSaver much easier.
      * Fields can implement a DSPropertyConverter interface to transparently be
        serialized to/from some other datastore-compatible type. This makes many
        uses of PropertyLoadSaver unnecessary.
      * Lots of reflection code moved out of structPLS.{Load,Save} into the creation
        of the struct codec (e.g. it happens once instead of on every load/save now).
      * Struct codecs are directly accessible by users, allowing easier
        implementation of PropertyLoadSaver interface.
      * Struct codecs support 'special' tagged fields. This will be used for
        implementing an embedded-key Datastore interface without adding any extra
        reflection code.
      * DSProperty's now have strictly private fields. This ensures that every
        DSProperty instance contains only valid types for Value (since it's not
        possible to construct one with an incompatible Value now), which simplifies
        error checking code.
      * DSProperty does automatic upconversion of close-but-not-exact types (like
        MyString -> string, and int -> int64). This upconversion was previously only
        available for struct serialization.
      * DSProperty's now retain a sortable 'Type' field to avoid unnecessary
        additional reflection when serializing them (such as to memcache).
      * There is now a consistent, sortable binary serialization format for
        DSPropertyMap's, which will be used for the follow-on transparent memcache
        implementation. It's also used internally by the in-memory testing
        implementation of the datastore.
    
    *I alias some error types from the new-SDK, but users of the abstraction no
    longer need to import the SDK at all.
    
    R=maruel@chromium.org, vadimsh@chromium.org
    BUG=
    
    Review URL: https://codereview.chromium.org/1222903002

--- testable.go
+++ testable.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package wrapper
+package gae
 
 // Testable is the basic interface that every fake service should implement.
 type Testable interface {
