e44d48d870688e5dd668fc136f1dc4fa8f5ada7b
Dan Jacques <dnj@google.com>
2017-08-31 15:46:42 -0700

    [datastore] Callbacks now supply an index.
    Callbacks currently assume sequential ordering. In order to support
    future batching in parallel, we need to enable callbacks to indicate
    the index of the entry they're operating on.
    
    BUG=None
    TEST=unit
    
    Change-Id: I684b8ec3fd85e2206d0358576be694105ff20ce0

--- service/datastore/raw_interface.go
+++ service/datastore/raw_interface.go
@@ -43,35 +43,50 @@ type RawRunCB func(key *Key, val PropertyMap, getCursor CursorCB) error
 
 // GetMultiCB is the callback signature provided to RawInterface.GetMulti
 //
+//   - idx is the index of the entity
 //   - val is the data of the entity
 //     * It may be nil if some of the keys to the GetMulti were bad, since all
 //       keys are validated before the RPC occurs!
 //   - err is an error associated with this entity (e.g. ErrNoSuchEntity).
 //
+// The callback is called once per element. It may be called concurrently, and
+// may be called out of order. The "idx" variable describes which element is
+// being processed.
+//
 // Return nil to continue iterating, or an error to stop. If you return the
 // error `Stop`, then GetMulti will stop the query and return nil.
-type GetMultiCB func(val PropertyMap, err error) error
+type GetMultiCB func(idx int, val PropertyMap, err error) error
 
 // NewKeyCB is the callback signature provided to RawInterface.PutMulti and
 // RawInterface.AllocateIDs. It is invoked once for each positional key that
 // was generated as the result of a call.
 //
+//   - int is the index of the entry
 //   - key is the new key for the entity (if the original was incomplete)
 //     * It may be nil if some of the keys/vals to the PutMulti were bad, since
 //       all keys are validated before the RPC occurs!
 //   - err is an error associated with putting this entity.
 //
+// The callback is called once per element. It may be called concurrently, and
+// may be called out of order. The "idx" variable describes which element is
+// being processed.
+//
 // Return nil to continue iterating, or an error to stop. If you return the
 // error `Stop`, then PutMulti will stop the query and return nil.
-type NewKeyCB func(key *Key, err error) error
+type NewKeyCB func(idx int, key *Key, err error) error
 
 // DeleteMultiCB is the callback signature provided to RawInterface.DeleteMulti
 //
+//   - idx is the index of the entity
 //   - err is an error associated with deleting this entity.
+//
+// The callback is called once per element. It may be called concurrently, and
+// may be called out of order. The "idx" variable describes which element is
+// being processed.
 //
 // Return nil to continue iterating, or an error to stop. If you return the
 // error `Stop`, then DeleteMulti will stop the query and return nil.
-type DeleteMultiCB func(err error) error
+type DeleteMultiCB func(idx int, err error) error
 
 // Constraints represent implementation constraints.
 type Constraints struct {
