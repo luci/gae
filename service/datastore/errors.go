2272a14603c8d7ef25f8ecd599f6f9e5737b13b6
Ryan Tseng <hinoka@google.com>
2018-05-04 10:48:09 -0700

    [datastore] Add IsErrNoSuchEntity in datastore/errors.go
    This was an unsurprisingly common thing to do.
    Also datastore already imports errors, so this was the most logical place to put this.
    
    Change-Id: I11f5ef777733de85cbd2a9616beab6500a47bf06

--- service/datastore/errors.go
+++ service/datastore/errors.go
@@ -48,6 +48,16 @@ func MakeErrInvalidKey(reason string, args ...interface{}) *errors.Annotator {
 // error.
 func IsErrInvalidKey(err error) bool { return errors.Unwrap(err) == datastore.ErrInvalidKey }
 
+// IsErrNoSuchEntity tests if an error is ErrNoSuchEntity,
+// or is a MultiError that contains ErrNoSuchEntity and no other errors.
+func IsErrNoSuchEntity(err error) (found bool) {
+	errors.Walk(err, func(err error) bool {
+		found = err == datastore.ErrNoSuchEntity
+		return found
+	})
+	return
+}
+
 // ErrFieldMismatch is returned when a field is to be loaded into a different
 // type than the one it was stored from, or when a field is missing or
 // unexported in the destination struct.
